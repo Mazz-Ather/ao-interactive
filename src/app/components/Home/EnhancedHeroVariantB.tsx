4ea622b69504457b62e9e9163fe2101dd2019bbe
Mazz-Ather <mazzather@gmail.com>
2025-09-10 15:04:27 +0500

    error resolved

--- src/app/components/Home/EnhancedHeroVariantB.tsx
+++ src/app/components/Home/EnhancedHeroVariantB.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { ExpertiseRotator } from './ExpertiseRotator';
+import ExpertiseRotator from './ExpertiseRotator';
 
 // Brand color from Navbar.tsx
 const BRAND_COLOR = '#40ED70'; // Green color used in navbar
